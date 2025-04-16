ce3b3e9da5b83789e077b7f335a435f47bce31b8
lucasgschagas <lucasgschagas@gmail.com>
2025-04-16 18:40:41 +0000

    feat: adicionando home screen e widgets

--- lib/ui/_core/app_colors.dart
+++ lib/ui/_core/app_colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 abstract class AppColors {
   static const Color backgroundColor = Color(0xFF202123);
   static const Color mainColor = Color(0xFFffa559);
+  static const Color lightBackgroudColor = Color(0xFF343541);
 }
---
+++ lib/ui/home/home_screen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/data/categories_data.dart';
+import 'package:myapp/data/restaurants_data.dart';
+import 'package:myapp/model/restaurant.dart';
+import 'package:myapp/ui/home/widgets/category_widget.dart';
+import 'package:myapp/ui/home/widgets/restaurant_widget.dart';
+import 'package:provider/provider.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    RestaurantData restaurantData = Provider.of<RestaurantData>(context);
+    return Scaffold(
+      drawer: Drawer(),
+      appBar: AppBar(
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.shopping_cart)),
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: SingleChildScrollView(
+          child: Column(
+            spacing: 32,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(child: Image.asset('assets/logo.png', width: 147)),
+              Text("Boas vindas!"),
+              TextFormField(),
+              Text("Escolha por categoria"),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  spacing: 8,
+                  children: List.generate(
+                    CategoriesData.listCategories.length,
+                    (index) {
+                      return CategoryWidget(
+                        category: CategoriesData.listCategories[index],
+                      );
+                    },
+                  ),
+                ),
+              ),
+              Image.asset("assets/banners/banner_promo.png"),
+              Text("Bem avaliados"),
+              Column(
+                spacing: 16,
+                children: List.generate(restaurantData.listRestaurants.length, (
+                  index,
+                ) {
+                  Restaurant restaurant = restaurantData.listRestaurants[index];
+                  return RestaurantWidget(restaurant: restaurant);
+                }),
+              ),
+              SizedBox(height: 64),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
